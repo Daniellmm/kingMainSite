d86c717e1cde23b3491b15d9608bc07c52858a8c
Daniellmm <idowudanielcsc190400@gmail.com>
2025-04-08 00:54:52 -1100

    done with the 3 page

--- src/pages/Insurance.jsx
+++ src/pages/Insurance.jsx
@@ -5,6 +5,11 @@ import NOTE2 from '../assets/images/bg-live/note2.png'
 import HEADPHONE from '../assets/images/headphone.png'
 import BIGLOGO from '../assets/images/biglogo.png'
 import FALLCOIN from '../assets/images/fallcoin.png'
+import LONG from '../assets/images/long.png'
+import KING from '../assets/images/king.png'
+import ALEXIS from '../assets/images/alexis.png'
+import GENE from '../assets/images/gene.png'
+
 
 const Insurance = () => {
   return (
@@ -135,6 +140,108 @@ const Insurance = () => {
                 <button className='bg-[#E7A647] px-5 py-3 text-sm rounded-[8px]' style={{ fontFamily: 'Montserrat, serif', fontWeight: 'medium' }}>
                   FREE Financial Review
                 </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section className='overflow-hidden flex flex-col justify-center items-center px-10 bg-black md:min-h-[80vh] pb-10'>
+        <div className='flex flex-col justify-center items-center pt-10 lg:pt-16 gap-y-3'>
+          <h1 className='text-xl font-thin tracking-widest text-gray-400 text-center'>Trusted by Professionals Like You</h1>
+
+          <h1 className='text-[#E7A647] text-center pb-10 text-3xl font-semibold' style={{ fontFamily: 'Minion Pro, serif' }}>Client Success Stories</h1>
+
+          <div>
+            <img src={LONG} alt="" />
+          </div>
+        </div>
+      </section>
+
+
+      <section className='overflow-hidden flex justify-center items-center px-10 bg-white min-h-[100vh] pb-14'>
+        <div className='flex flex-col justify-center items-center pt-10'>
+          <div>
+            <h1 className='text-[#E7A647] text-center pb-10 text-3xl font-semibold' style={{ fontFamily: 'Minion Pro, serif' }}>Meet the Team</h1>
+          </div>
+
+          <div className='lg:px-20 space-y-10'>
+            <div className='grid md:grid-cols-2 space-y-10'>
+              <div>
+                <img src={KING} alt="" />
+              </div>
+
+              <div className='flex flex-col justify-center items-start'>
+                <div>
+                  <h1 className='text-[#E7A647] text-2xl text-start' style={{ fontFamily: 'Montserrat, serif', fontWeight: 'medium' }}>King Edwards</h1>
+                </div>
+                <p className='text-lg italic font-semibold' style={{ fontFamily: 'Montserrat, serif', fontWeight: 'medium' }}>King, a visionary, funding expert, and author</p>
+
+                <p className='text-sm pt-5'>
+                  was born in Los Angeles and raised in New York. He draws inspiration from the diverse influences of each city. LA exposed him to the glamorous Hollywood lifestyle, while New York instilled a sense of grit and determination.
+                  <br /> <br />
+                  In his first professional project, King embarked on the creation of HighTicketOfferFinancing.com. This innovative venture aimed to provide alternative funding solutions for entrepreneurs operating in industries traditionally overlooked by banks. Through this endeavor, he sought to help consultants and entrepreneurs earn significant profits, develop and sell high-value products and services, and safeguard themselves from legal implications using HTOF’s comprehensive workflows, standard operating procedures, and contract templates.
+                  <br /> <br />
+                  With a focus on helping entrepreneurs thrive and achieve remarkable financial milestones, King has received accolades such as the Man of the Moment and Made Man Award for his achievements. Through unwavering determination and an unwavering focus on the next best move, King aims to rewrite the rules of the game and help his clients leave an indelible mark on their respective industries and their family’s lives.
+                </p>
+              </div>
+            </div>
+
+            <div className='grid md:grid-cols-2 space-y-10'>
+              <div>
+                <img src={ALEXIS} alt="" />
+              </div>
+
+              <div className='flex flex-col justify-center items-start'>
+                <div>
+                  <h1 className='text-[#E7A647] text-2xl text-start' style={{ fontFamily: 'Montserrat, serif', fontWeight: 'medium' }}>Alexis Holifield</h1>
+                </div>
+                <p className='text-lg italic font-semibold' style={{ fontFamily: 'Montserrat, serif', fontWeight: 'medium' }}>Co-Founder of The Millennial Millionaire</p>
+
+                <p className='text-sm pt-5'>
+                  Alexis Holifield, began her journey to learning the laws of building generational wealth at the early age of 17 years old. She began her journey by attending a seminar put on by T. Harv Eker "Secrets Of The Millionaire Mind". Immediately, she began creating an archive of these "well-kept secrets of the wealthy" for the world to know. She went on to graduate with honors and complete a masters degree in life through first hand experience of wealth building, in addition the Multi-Millionaires she continues to be mentored by.
+                  <br /> <br />
+                  As a Licensed Insurance Broker, Financial Strategist, & Wealth Planner, Alexis dedicates her time as a coach and a mentor sharing Financial Intelligence and Investment Education to cultivate the minds of Millennials all over the nation. Her mission to advise families, individuals, and business owners to begin or enhance their journey to financial freedom to increase and preserve wealth is expressed in her ability to transform the paradigm, and financial states of her client. Specializing in Estate Planning, Cash Value Life Insurance, Retirement, and Executive Business Planning.
+                </p>
+              </div>
+            </div>
+
+            <div className='flex justify-center items-center'>
+              <div className='grid md:grid-cols-2 space-y-10'>
+                <div>
+                  <img src={GENE} alt="" />
+                </div>
+
+                <div className='flex flex-col justify-center items-start'>
+                  <div>
+                    <h1 className='text-[#E7A647] text-2xl text-start' style={{ fontFamily: 'Montserrat, serif', fontWeight: 'medium' }}>Gene Santos</h1>
+                  </div>
+                  <p className='text-lg italic font-semibold' style={{ fontFamily: 'Montserrat, serif', fontWeight: 'medium' }}>Financial Advisor</p>
+
+                  <p className='text-sm pt-5'>
+                    Gene Santos is a distinguished financial advisor specializing in comprehensive wealth
+                    management for high net worth individuals and families. With over a decade of strategic experience in the finance and insurance industry, Gene has established himself as a trusted
+                    advisor to discerning clients seeking sophisticated financial solutions.
+                    <br /> <br />
+                    Demonstrating exceptional performance, Gene has successfully written over $25 million in life
+                    insurance coverage, strategically managed in excess of $2.4 million in client assets, and
+                    facilitated more than $15 million in annuity premiums. His practice is precisely calibrated to
+                    serve high net worth clients, delivering nuanced financial strategies that address the complex
+                    wealth management needs of successful professionals and entrepreneurs.
+                    <br /> <br />
+                    Specializing in advanced retirement planning, comprehensive estate strategies, and tailored
+                    investment optimization, Gene provides a strategic approach that goes beyond standard
+                    financial advising. His expertise lies in crafting customized financial solutions that protect and
+                    grow wealth, with a keen understanding of the unique financial challenges facing high-net-worth
+                    individuals.
+                    <br /> <br />
+                    Gene's client-focused methodology combines rigorous financial acumen with a commitment to
+                    building long-term, trust-based relationships. By offering personalized guidance and strategic
+                    insights, he empowers clients to make informed decisions that align with their most significant
+                    financial objectives and legacy planning goals.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
