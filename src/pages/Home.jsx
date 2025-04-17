e55390c19b10f370e025619b6ebb53988546355b
Daniellmm <idowudanielcsc190400@gmail.com>
2025-04-17 01:18:13 -1100

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,8 +75,8 @@ const Home = () => {
   return (
     <>
 
-      <section className='relative overflow-hidden pb-10 px-10 bg-black flex justify-center items-center min-h-screen md:rounded-br-[100px] md:rounded-bl-[100px]'>
-        <div className='z-10 relative pt-32 w-full flex flex-col justify-center items-center'>
+      <section className='relative overflow-hidden pb-10 px-10 bg-black flex justify-center items-center md:min-h-screen md:rounded-br-[100px] md:rounded-bl-[100px]'>
+        <div className='z-10 relative pt-20 w-full flex flex-col justify-center items-center'>
           <ScrollAnimation animation="zoomIn" delay={0.5}>
             <div className='pb-7'>
               {/* <img src={MText} alt="" /> */}
